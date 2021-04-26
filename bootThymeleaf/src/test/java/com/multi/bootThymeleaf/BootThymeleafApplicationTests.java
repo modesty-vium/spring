837b795c05221d008f6a14b94fcdf0e728e0578d
modesty vium <citizen_politics@naver.com>
2021-04-26 15:36:44 +0900

    Initial Upload

---
+++ bootThymeleaf/src/test/java/com/multi/bootThymeleaf/BootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.bootThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
